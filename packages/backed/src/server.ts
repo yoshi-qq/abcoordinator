da11f6a900e809d9058856a8e440238d956bc11c
Ben <ben.thiemann@icloud.com>
2025-11-21 22:04:21 +0100

    added important files and folders (backend/frontend structure) package.json tsconfig.json in backend tsconfig.json in root (temporary server.ts file) app.tsx

---
+++ packages/backed/src/server.ts
@@ -0,0 +1,13 @@
+// backend/src/server.ts
+import express from 'express';
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
