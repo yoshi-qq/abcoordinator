755e95e273b76146da17280d3f40aad271e72bf9
Ben <ben.thiemann@icloud.com>
2025-11-21 23:19:39 +0100

    minor changes

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+// @ts-expect-error missing types
 import "./globals.css";
 
 const geistSans = Geist({
---
+++ packages/frontend/src/components/Calendar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Calendar = (props: Props) => {
+  return (
+    <div>TopBar</div>
+  )
+}
+
+export default Calendar;
